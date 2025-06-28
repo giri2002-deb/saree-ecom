7e1b1cf3546a2fc20f1bf6fbc195ed728921a111
giri2002-deb <140328477+giri2002-deb@users.noreply.github.com>
2025-06-28 09:55:52 +0530

    Initial full project upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+
+import Shop from './components/Shop';
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div className="min-h-screen bg-white">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+              <Route path="/Shop" element={<Shop />} />
+               
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
